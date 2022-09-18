ebdb4f1520c4a60f9ab174315d702edeb52400ba
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2022-09-18 18:55:38 +0530

    import java.util.Scanner; class Sample { 	public static void main(String[] args) 	{ 		// TODO Auto-generated method stub 		Scanner sc=new Scanner(System.in); 		int n; 		n=sc.nextInt();
    		for(int i=1;i<=n;i++)			// rows 
    		{
    			for(int j=1;j<=n;j++)		// cols 
    			{
    				System.out.print(n-(j-1)+" ");
    			}
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 class Sample
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int N;
-        N=sc.nextInt();
-        for( int i=N;i<=N && i>0;i--)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-    }
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++)			// rows 
+		{
+			for(int j=1;j<=n;j++)		// cols 
+			{
+				System.out.print(n-(j-1)+" ");
+			}
+			System.out.println();
+		}
+	}
 }

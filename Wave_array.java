c34df806f5908b48cf9bead31145c06d5a424416
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2022-10-24 09:34:30 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],c=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();
            for(int i=1;i<n-1;i++)
            {
                if(x[i+1]>x[i] && x[i-1]>x[i])
                c++;
                else if(x[i+1]<x[i] && x[i-1]<x[i])
                {
                    c++;
                    
                }
            }
            if(c==n-2)
            {
                System.out.println("yes");
                
            }
            else
            {
                System.out.println("no");
            }
        }
    }

---
+++ Wave_array.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        
+        for(int i=1;i<n-1;i++)
+        {
+            if(x[i+1]>x[i] && x[i-1]>x[i])
+            c++;
+            else if(x[i+1]<x[i] && x[i-1]<x[i])
+            {
+                c++;
+                
+            }
+        }
+        if(c==n-2)
+        {
+            System.out.println("yes");
+            
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}

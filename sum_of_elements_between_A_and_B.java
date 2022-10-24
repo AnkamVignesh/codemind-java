561a7072c2658cbc2d25a808b0252984a9c76a2c
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2022-10-24 11:00:02 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],sum=0,a,b,i;         n=sc.nextInt();         x=new int[n];         for (i=0;i<n;i++)         x[i]=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();
            for(i=0;i<n;i++)
            {
                if(x[i]>=a && x[i]<=b)
                {
                    sum=sum+x[i];
                }
            }
            
              System.out.println(sum);
           
            
        }
    }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],sum=0,a,b,i;
+        n=sc.nextInt();
+        x=new int[n];
+        for (i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        a=sc.nextInt();
+        b=sc.nextInt();
+        
+        
+        for(i=0;i<n;i++)
+        {
+            if(x[i]>=a && x[i]<=b)
+            {
+                sum=sum+x[i];
+            }
+        }
+        
+          System.out.println(sum);
+       
+        
+    }
+}

f6fccd48a36675b9c923b29e0144cef93b5f22e8
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2022-10-25 17:12:12 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,x[],count=0,flag=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         int k=sc.nextInt();
            for(i=0;i<n;i++)
            {
                count=1;
                if(x[i]!=-99)
                {
                    for(j=0;j<n;j++)
                    {
                        if(x[i]==x[j] && i!=j)
                        {
                            count++;
                            x[j]=-99;
                        }
                    }
                }
                
                if(k==count)
                {
                   System.out.print(x[i]+" ");
                   flag=1;
                }
            }
           if(flag==0)
              
              System.out.println("-1");
            
            
        }
    }

---
+++ N_times_repeated_elements.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,x[],count=0,flag=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        int k=sc.nextInt();
+        
+        
+        for(i=0;i<n;i++)
+        {
+            count=1;
+            if(x[i]!=-99)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(x[i]==x[j] && i!=j)
+                    {
+                        count++;
+                        x[j]=-99;
+                    }
+                }
+            }
+            
+            if(k==count)
+            {
+               System.out.print(x[i]+" ");
+               flag=1;
+            }
+        }
+       if(flag==0)
+          
+          System.out.println("-1");
+        
+        
+    }
+}

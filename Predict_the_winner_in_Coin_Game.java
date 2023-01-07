fe1cb65eddf2301142e7e8f0535c32a86e48f2cf
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2023-01-07 18:26:10 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n;         m=sc.nextInt();         n=sc.nextInt();
            if(n%2==0)
            {
               m=0;
               System.out.println("Player 1");
                
            }
            else
              System.out.println("Player 2");
              
        }
    }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        
+        if(n%2==0)
+        {
+           m=0;
+           System.out.println("Player 1");
+            
+        }
+        else
+          System.out.println("Player 2");
+          
+    }
+}

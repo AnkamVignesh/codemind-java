ea5611047992b7f2a55a35de511973ba7e15e483
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2023-01-12 15:55:06 +0530

    import java.util.Scanner; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,r,max=0;         n=sc.nextInt();
            while(n>0)
            {
                r=n%10;
                if(max<r)
                 max=r;
                 n=n/10;
            }
            System.out.println(max);
        }
    }

---
+++ Largest_Digit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,r,max=0;
+        n=sc.nextInt();
+        
+        while(n>0)
+        {
+            r=n%10;
+            if(max<r)
+             max=r;
+             n=n/10;
+        }
+        System.out.println(max);
+    }
+}

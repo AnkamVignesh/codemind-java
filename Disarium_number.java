300b9eedddd1ec933cedd6e28a38a4a2d000020e
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2023-01-18 19:14:18 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,d,r,sum=0;         n=sc.nextInt();         int temp=n;
            d=(int)Math.log10(n)+1;
            while(n>0)
            {
                r=n%10;
                sum=sum+(int)Math.pow(r,d);
                d--;
                n=n/10;
                
            }
            
            if(sum==temp)
              System.out.println("True");
            else
              System.out.println("False");
        }
    }

--- Disarium_number.java
+++ Disarium_number.java
@@ -1,32 +1,26 @@
 import java.util.Scanner;
 class Main
 {
-    public static boolean disarian(int n)
+    public static void main(String args[])
     {
-        int sum=0,r;
+        Scanner sc=new Scanner(System.in);
+        int n,d,r,sum=0;
+        n=sc.nextInt();
         int temp=n;
-        int d=(int)Math.log10(n)+1;
         
+        d=(int)Math.log10(n)+1;
         while(n>0)
         {
             r=n%10;
             sum=sum+(int)Math.pow(r,d);
             d--;
             n=n/10;
+            
         }
+        
         if(sum==temp)
-         return true;
-        else
-         return false;
-    }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        if(disarian(n))
           System.out.println("True");
         else
           System.out.println("False");
     }
-    
 }

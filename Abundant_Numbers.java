f716fc0eb3b0214f7a84ced5a55b358b48689a12
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2023-01-18 20:43:51 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);          int n,sum=0,i;          n=sc.nextInt();
             for(i=1;i<=n/2;i++)
             {
                 if(n%i==0)
                 {
                     sum=sum+i;
                 }
             }
             if(sum>n)
              System.out.println("True");
             else
               System.out.println("False");
        }
    }

--- Abundant_Numbers.java
+++ Abundant_Numbers.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-class main
+class Main
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,sum=0,i;
-        n=sc.nextInt();
-        for(i=1;i<=n/2;i++)
-        {
-            if(n%i==0)
-            {
-                sum=sum+i;
-            }
-        }
-        if(sum>n)
+         int n,sum=0,i;
+         n=sc.nextInt();
+         
+         for(i=1;i<=n/2;i++)
+         {
+             if(n%i==0)
+             {
+                 sum=sum+i;
+             }
+         }
+         if(sum>n)
           System.out.println("True");
-        else
+         else
            System.out.println("False");
     }
 }

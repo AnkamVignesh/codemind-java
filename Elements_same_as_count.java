8cb92bf360455fcff025f54b27a4d432f87a18cf
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2023-01-22 21:17:05 +0530

    import java.util.*; import java.util.Map.*; class Main {     public static void aaa(int a[])     {         int sum=0,count=0;         HashMap<Integer,Integer> hm=new HashMap<>();         for(int i=0;i<a.length;i++)         {             hm.put(a[i],hm.getOrDefault(a[i],0)+1);         }         for(int i=0;i<a.length;i++)         {             if(a[i]==(int)hm.get(a[i]))             {                System.out.print(a[i]+" ");                hm.put(a[i],0);                count++;             }         }         if(count==0)          System.out.println(-1);     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)           x[i]=sc.nextInt();        aaa(x);     } }

--- Elements_same_as_count.java
+++ Elements_same_as_count.java
@@ -1,42 +1,37 @@
-import java.util.Scanner;
+
+
+import java.util.*;
+import java.util.Map.*;
 class Main
 {
+    public static void aaa(int a[])
+    {
+        int sum=0,count=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<a.length;i++)
+        {
+            hm.put(a[i],hm.getOrDefault(a[i],0)+1);
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==(int)hm.get(a[i]))
+            {
+               System.out.print(a[i]+" ");
+               hm.put(a[i],0);
+               count++;
+            }
+        }
+        if(count==0)
+         System.out.println(-1);
+    }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,i,j,x[],count=0,flag=0;
+        int n,x[];
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
-        x[i]=sc.nextInt();
-       
-        
-        
-        for(i=0;i<n;i++)
-        {
-            count=1;
-            if(x[i]!=-99)
-            {
-                for(j=0;j<n;j++)
-                {
-                    if(x[i]==x[j] && i!=j)
-                    {
-                        count++;
-                        x[j]=-99;
-                    }
-                }
-            }
-            
-            if(x[i]==count)
-            {
-               System.out.print(x[i]+" ");
-               flag=1;
-            }
-        }
-        if(flag==0)
-          
-          System.out.println("-1");
-        
-        
+        for(int i=0;i<n;i++)
+          x[i]=sc.nextInt();
+       aaa(x);
     }
 }

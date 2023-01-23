350ba666945bb18f766203df48eacfab8d3116d7
AnkamVignesh <112751345+AnkamVignesh@users.noreply.github.com>
2023-01-23 21:41:26 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         if(n==1)         return false;
            for(int i=2;i<(int)Math.sqrt(n);i++)
            {
                if(n%i==0)
                return false;
            }
            return true;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
          int n,found=0;
          n=sc.nextInt();
           if(n>2)
           {
            for(int i=2;i<=(int)n/2;i++)
            {
                if(prime(i))
                {
                    for(int j=2;j<=(int)n/2;j++)
                    {
                        if(prime(j))
                        {
                            if(i*j==n && i!=j)
                            {
                                System.out.print(i+" ");
                                found=1;
                                break;
                            }
                        }
                    }
                }
            }
            
        }
        if(found==0 || n<2)
          System.out.println(-1);
        }
    }

--- Find_two_distinct_prime_numbers_with_given_product.java
+++ Find_two_distinct_prime_numbers_with_given_product.java
@@ -5,6 +5,7 @@ class Main
     {
         if(n==1)
         return false;
+        
         for(int i=2;i<(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
@@ -15,35 +16,31 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,found=0;
-        n=sc.nextInt();
-        
-        if(n>2)
+      int n,found=0;
+      n=sc.nextInt();
+       if(n>2)
+       {
+        for(int i=2;i<=(int)n/2;i++)
         {
-            for(int i=2;i<=(int)n/2;i++)
+            if(prime(i))
             {
-                if(prime(i))
+                for(int j=2;j<=(int)n/2;j++)
                 {
-                    for(int j=2;j<=(int)n/2;j++)
+                    if(prime(j))
                     {
-                        if(prime(j))
+                        if(i*j==n && i!=j)
                         {
-                            if(i*j==n && i!=j)
-                            {
-                                System.out.print(i+" ");
-                                found=1;
-                                break;
-                            }
+                            System.out.print(i+" ");
+                            found=1;
+                            break;
                         }
                     }
-                    
-                   
                 }
-                
             }
-           
         }
-        if(found==0 || n<2)
-                      System.out.println(-1);
+        
+    }
+    if(found==0 || n<2)
+      System.out.println(-1);
     }
 }
